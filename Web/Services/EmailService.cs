1edd5c5acbe6e8f8c1564b5fa93c90918dd400e2
magaliperlin237 <magaliperlin237@gmail.com>
2026-02-04 21:11:02 +0900

    Add using directives for HttpClient and JSON

--- Web/Services/EmailService.cs
+++ Web/Services/EmailService.cs
@@ -3,6 +3,14 @@ using System.Net;
 using Web.Models;
 using Microsoft.Extensions.Options;
 
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
 namespace Web.Services
 {
     /// <summary>
